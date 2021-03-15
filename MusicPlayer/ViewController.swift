e4eccd542a07e5cef3b3470de8281c518e2aef54
Titov, Evgeny <e.titov@itransition.com>
2021-03-15 19:48:28 +0300

    initial commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Titov, Evgeny on 15.03.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
